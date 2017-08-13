3a21fc927ccef9e38fcb642d9006a9e0dce9886b
sn0wf1llin <sn0wf1llin@gmail.com>
2017-08-13 17:53:18 +0300

    Add oddName

--- oddName.py
+++ oddName.py
@@ -1,2 +1,18 @@
 """Author"""
 
+
+def main():
+    print("Input your name: ")
+    name = input()
+    s = ''
+    if len(name) != 0:
+        for i in range(1, len(name)):
+            if i % 2 == 1:
+                s += name[i]
+        print(s)
+
+    else:
+        print("Error")
+
+if __name__ == "__main__":
+    main()
